c66df72a79db32d36c5f5a6ebb423b0d4384dc15
Niklas Mollenhauer <nikeee@users.noreply.github.com>
2026-09-24 21:02:06 +0200

    Add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nikeee.github.io/golden-hour',
+  site: 'https://nikeee.github.io',
+  base: '/golden-hour',
 });
